95ce5dcf164a0f2d8d9e2fcf451bc4ba88ca6ec9
Marjorie <marjoriesiad@gmail.com>
2022-10-25 10:23:42 +0200

    création sidebar component

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import yogaIcon from "../../assets/sidebar_1.svg";
 import swimIcon from "../../assets/sidebar_2.svg";
 import bikeIcon from "../../assets/sidebar_3.svg";
-import weightogaIcon from "../../assets/sidebar_4.svg";
+import weightIcon from "../../assets/sidebar_4.svg";
+
+const Sidebar = () => {
+    return (
+        <aside className="sidebar">
+            <nav>
+                <Link to="/yoga"><img src={yogaIcon} alt="Icon Yoga" /></Link>
+                <Link to="/nage"><img src={swimIcon} alt="Icon Nage" /></Link>
+                <Link to="/velo"><img src={bikeIcon} alt="Icon Vélo" /></Link>
+                <Link to="/musculation"><img src={weightIcon} alt="Icon Musculation" /></Link>
+            </nav>
+        </aside>
+    );
+};
+
+export default Sidebar;
